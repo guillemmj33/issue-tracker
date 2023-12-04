ad726cc9f8193de038c8e2c8622afa9f964c33b2
Guillem <guillemmj33@gmail.com>
2023-12-04 16:48:24 +0100

    Add @radix-ui/themes dependency and update UI components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Navbar from "./navbar"
+import { Theme } from "@radix-ui/themes"
+import "@radix-ui/themes/styles.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main className="p-5">{children}</main>
+        </Theme>
       </body>
     </html>
   )
