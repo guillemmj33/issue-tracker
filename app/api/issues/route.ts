912b152fd5088b509c16e10515b9bf67fa08eb4f
Guillem <guillemmj33@gmail.com>
2023-12-04 16:36:43 +0100

    creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server"
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client"
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1).max(255),
+})
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  const validation = createIssueSchema.safeParse(body)
+  const prisma = new PrismaClient()
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 })
+  }
+
+  try {
+    const newIssue = await prisma.issue.create({
+      data: { title: body.title, description: body.description },
+    })
+
+    return NextResponse.json(newIssue, { status: 201 })
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create issue" },
+      { status: 500 }
+    )
+  } finally {
+    await prisma.$disconnect()
+  }
+}
