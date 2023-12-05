e7c15622dbe5c6a1c72626aa1f03b8e314d75f4f
Guillem <guillemmj33@gmail.com>
2023-12-05 16:20:15 +0100

    Imported ErrorMessage, dynamic, Spinner, createIssueSchema, zodResolver, Button, Callout, TextField, axios, useRouter, useState, Controller, useForm, and SimpleMDE components

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,20 @@
 "use client"
+import ErrorMessage from "@/app/components/ErrorMessage"
+import dynamic from 'next/dynamic'
+import Spinner from "@/app/components/Spinner"
+import { createIssueSchema } from "@/app/validationSchemas"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button, Callout, TextField } from "@radix-ui/themes"
-import SimpleMDE from "react-simplemde-editor"
-import "easymde/dist/easymde.min.css"
 import axios from "axios"
-import { useForm, Controller } from "react-hook-form"
+import "easymde/dist/easymde.min.css"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
-import { createIssueSchema } from "@/app/validationSchemas"
+import { Controller, useForm } from "react-hook-form"
 import { z } from "zod"
-import ErrorMessage from "@/app/components/ErrorMessage"
-import Spinner from "@/app/components/Spinner"
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+})
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
