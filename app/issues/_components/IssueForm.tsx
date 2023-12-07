734d5fad01406dd85667360c38db27ad9d96d0ed
Guillem <guillemmj33@gmail.com>
2023-12-07 17:24:11 +0100

    Refactor issue form submission logic

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -35,7 +35,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setIsSubmitting(true)
-      await axios.post("/api/issues", data)
+      if (issue) await axios.patch(`/api/issues/${issue.id}`, data)
+      else await axios.post("/api/issues", data)
       router.push("/issues")
     } catch (error) {
       setIsSubmitting(false)
@@ -52,7 +53,11 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       )}
       <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
-          <TextField.Input defaultValue={issue?.title} placeholder="Title" {...register("title")} />
+          <TextField.Input
+            defaultValue={issue?.title}
+            placeholder="Title"
+            {...register("title")}
+          />
         </TextField.Root>
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
@@ -65,7 +70,9 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue{isSubmitting && <Spinner />}
+          {issue ? "Update Issue" : "Submit New Issue"}
+          {""}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
