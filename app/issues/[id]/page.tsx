7f07e99af1ab71ea8da5ef09e5581253e3283da1
Guillem <guillemmj33@gmail.com>
2023-12-19 14:25:17 +0100

    Refactor AssigneeSelect component and import dependencies
    This commit refactors the AssigneeSelect component in the app/issues/[id]/AssigneeSelect.tsx file. It imports the User type from the @prisma/client module and the axios, useEffect, and useState functions from their respective modules. It also adds a fetchUsers function that makes a GET request to the /api/users endpoint and sets the fetched data to the users state. The Select.Item component is updated to dynamically render the users' names. Additionally, the ASsigneeSelect component is renamed to AssigneeSelect.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,12 @@
+import authOptions from "@/app/api/auth/authOptions"
 import prisma from "@/prisma/client"
 import { Box, Flex, Grid } from "@radix-ui/themes"
+import { getServerSession } from "next-auth"
 import { notFound } from "next/navigation"
+import AssigneeSelect from "./AssigneeSelect"
+import DeleteIssueButton from "./DeleteIssueButton"
 import EditIssueButton from "./EditIssueButton"
 import IssueDetails from "./IssueDetails"
-import DeleteIssueButton from "./DeleteIssueButton"
-import { getServerSession } from "next-auth"
-import authOptions from "@/app/api/auth/authOptions"
-import ASsigneeSelect from "./ASsigneeSelect"
 
 interface Props {
   params: { id: string }
@@ -35,7 +35,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction="column" gap="4">
-            <ASsigneeSelect />
+            <AssigneeSelect />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
