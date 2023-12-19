f9596a333f27efd7689d21bd30078e9a8e744700
Guillem <guillemmj33@gmail.com>
2023-12-19 16:30:53 +0100

    Refactor AssigneeSelect component to accept issue prop

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,11 +1,11 @@
 "use client"
-import { User } from "@prisma/client"
+import { Issue, User } from "@prisma/client"
 import { Select } from "@radix-ui/themes"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import Skeleton from "react-loading-skeleton"
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -20,16 +20,24 @@ const AssigneeSelect = () => {
     retry: 3,
   })
 
-  if(isLoading) return <Skeleton />
+  if (isLoading) return <Skeleton />
 
   if (error) return null
 
   return (
-    <Select.Root>
+    <Select.Root
+      defaultValue={issue.assignedToUserId || "unassigned"}
+      onValueChange={(userId) => {
+        axios.patch("/api/issues/" + issue.id, {
+          assignedToUserId: userId === "unassigned" ? null : userId,
+        })
+      }}
+    >
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="unassigned">Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
