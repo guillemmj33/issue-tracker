72541e4409390e57c4870c8bf35cd941c5ffce7c
Guillem <guillemmj33@gmail.com>
2024-01-02 10:39:26 +0100

    Add IssueChart component and recharts dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import IssueChart from "./IssueChart"
 import IssueSummary from "./IssueSummary"
 import prisma from "@/prisma/client"
 
@@ -21,5 +22,5 @@ export default async function Home({
       status: "CLOSED",
     },
   })
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />
 }
